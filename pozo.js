a06d395668a2d962e218f1788978df04d219aab0
Luis Toledo <dti@alacoohperu.pe>
2023-03-07 16:40:16 -0500

    cambios en clima

--- pozo.js
+++ pozo.js
@@ -41,7 +41,7 @@ async function init() {
   } else if (text_clima == 'cielo claro' || text_clima == 'algo de nubes'|| text_clima == 'nubes dispersas') {
     content4.style.display = "block";
     
-  } else if (text_clima == 'lluvia ligera' || text_clima == 'tormenta con lluvia ligera'|| text_clima == 'lluvia moderada') {
+  } else if (text_clima == 'lluvia ligera' || text_clima == 'tormenta con lluvia ligera'|| text_clima == 'lluvia moderada' || text_clima == 'tormenta') {
     content5.style.display = "block";
 
   }else if (text_clima == 'nubes') {
